ab4ef77a8418733c74a63c402b507009294b32bd
maya lennox <2035059@cgmatane.qc.ca>
2021-03-19 14:52:04 -0400

    semaine 7

--- administration/liste-mission-apollo.php
+++ administration/liste-mission-apollo.php
@@ -13,8 +13,9 @@ $listeMissionApollo = $requeteListeMissionApollo->fetchAll();
 <html lang="fr">
 
 <head>
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
-    <link rel="stylesheet" href="../include/style.css" />
+    <link rel="stylesheet" href="style.css" />
     <meta charset="utf-8" />
     <title>La lune</title>
 </head>
@@ -22,20 +23,20 @@ $listeMissionApollo = $requeteListeMissionApollo->fetchAll();
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">La lune</a>
+            <a class="navbar-brand" href="index.php">La lune</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Acueil</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Acueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="../liste-mission-apollo.php">Les missions Apollo</a>
+                        <a class="nav-link active" href="liste-mission-apollo.php">Les missions Apollo</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="administration/liste-mission-apollo.php">Panneau admin</a>
+                        <a class="nav-link active" href="membre.php">Page membre</a>
                     </li>
                 </ul>
                 <form class="d-flex">
@@ -43,6 +44,7 @@ $listeMissionApollo = $requeteListeMissionApollo->fetchAll();
                     <button class="btn btn-outline-primary" type="submit">Rechercher</button>
                 </form>
             </div>
+        </div>
     </nav>
 
     <section>
--- configuration.php
+++ configuration.php
@@ -1,3 +1,3 @@
 <?php
 session_start();
-define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"] . "/projet-web-dynamique-2021-Mayo1590/accesseurs/");
+define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"] . "etudiants/2020/lennoxm/projet-web-dynamique-2021-Mayo1590/accesseurs");
